4a80a2464e09cffbb21040371f4576e9000a89e6
Tim Coote <tim@coote.org>
2019-11-29 23:39:11 +0000

    python3 print in y.py

--- y.py
+++ y.py
@@ -10,7 +10,7 @@ eye_cascade = cv2.CascadeClassifier('/usr/share/OpenCV/haarcascades/haarcascade_
 
 def detect (filename):
 
-    print filename
+    print (filename)
     img = cv2.imread(filename)
     #img = cv2.imread('IMG_20150905_163110.jpg')
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -18,7 +18,7 @@ def detect (filename):
     # cv3 has newer forms of this method to return more values
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=2, minSize=(30,30))
 
-    print len(faces), faces
+    print (len(faces), faces)
 
     for (x,y,w,h) in faces:
         cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
@@ -28,7 +28,7 @@ def detect (filename):
 #        eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.2, minNeighbors=2, minSize=(1,1))
 #        eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.2, minNeighbors=3, minSize=(1,1))
         eyes = eye_cascade.detectMultiScale(roi_gray, scaleFactor=1.1, minNeighbors=3, minSize=(1,1))
-        print len(eyes), eyes
+        print (len(eyes), eyes)
         for (ex,ey,ew,eh) in eyes:
             cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
 
